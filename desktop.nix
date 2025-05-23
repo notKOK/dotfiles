b53940f40bad290609ccf44bb38d42aa88f5cb5f
danila <a.deni.i@mail.ru>
2025-05-24 02:59:26 +0300

    26 current  2025-05-24 02:59:18  25.05.20250522.55d1f92     6.12.30                          *

--- desktop.nix
+++ desktop.nix
@@ -4,31 +4,33 @@
   inputs,
   ...
 }: {
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-  #services.xserver.videoDrivers = [ "amdgpu" ];
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  # services.displayManager.sddm.enable = true;
+  ## Enable the X11 windowing system.
+  ## You can disable this if you're only using the Wayland session.
+  #services.xserver.enable = true;
 
-  services.desktopManager.plasma6.enable = true;
-  #services.displayManager.sddm.wayland.enable = true;
-  programs.xwayland.enable = true;
-  services.displayManager.defaultSession = "plasmax11";
-  #services.displayManager.cosmic-greeter.enable = true;
-  #services.desktopManager.cosmic.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
+  ## Enable the KDE Plasma Desktop Environment.
+  ## services.displayManager.sddm.enable = true;
+
+  #services.desktopManager.plasma6.enable = true;
+  ##services.displayManager.sddm.wayland.enable = true;
+  #programs.xwayland.enable = true;
+  #services.displayManager.defaultSession = "plasmax11";
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.displayManager.gdm.wayland = true;
 
   # services.xserver.desktopManager.cinnamon.enable = true;
+
+  # Applications that lives only in Desktop Environment
   environment.systemPackages = with pkgs; [
     alacritty
   ];
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  ## Configure keymap in X11
+  #services.xserver.xkb = {
+  #  layout = "us";
+  #  variant = "";
+  #};
 }
